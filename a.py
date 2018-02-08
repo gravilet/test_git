ec1b27069a616dcffb9d6e287c3699a02253614e
Anton Kovalenko <anton.kovalenko@upmtelecom.com>
2018-02-08 22:43:57 +0300

    test_2

---
+++ a.py
@@ -0,0 +1,21 @@
+tttt
+Index: 2.txt
+IDEA additional info:
+Subsystem: com.intellij.openapi.diff.impl.patch.CharsetEP
+<+>UTF-8
+===================================================================
+--- 2.txt	(revision d45f1edc49a069a5838b18ad0901a7503248ff56)
++++ 2.txt	(revision d45f1edc49a069a5838b18ad0901a7503248ff56)
+@@ -0,0 +1,1 @@
++second test
+\ No newline at end of file
+Index: test.txt
+IDEA additional info:
+Subsystem: com.intellij.openapi.diff.impl.patch.CharsetEP
+<+>UTF-8
+===================================================================
+--- test.txt	(revision fd97d332d678675bb70722c7167ad14b372d3d2c)
++++ test.txt	(revision fd97d332d678675bb70722c7167ad14b372d3d2c)
+@@ -0,0 +1,1 @@
++test string
+\ No newline at end of file
